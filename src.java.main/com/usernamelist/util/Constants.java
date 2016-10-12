35e342a74adc118b81d6aa93188b8c5d41363b85
Niartsa <ronaldlizano1010@gmail.com>
2016-10-12 01:13:52 -0500

    Create Constants.java

---
+++ src.java.main/com/usernamelist/util/Constants.java
@@ -0,0 +1,12 @@
+package com.usernamelist.util;
+
+public class Constants {
+
+	public static final String RESOURCES_USERNAMES = "src.java.rsc/Usernames.dat";
+	public static final String RESOURCES_RESTRICTED = "src.java.rsc/Restricted.dat";
+	public static final String APPLICATION_CONTEXT = "ApplicationContext.xml";
+	public static final String USERNAMEGENERATOR_BEAN = "usernameGenerator";
+	public static final int USERNAMEMINIMUNSIZE = 6;
+	public static final int NUMBERATTEMPTSSUGGESTUSERS = 3;
+	public static final int NUMBERSUGGESTUSERS = 14;
+}
